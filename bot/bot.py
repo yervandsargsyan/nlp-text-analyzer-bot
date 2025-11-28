16e17a198682db62a65acdee055978b5e70eb315
yervandsargsyan <84045761+yervandsargsyan@users.noreply.github.com>
2025-11-28 20:15:35 +0400

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
 
 from telegram import Update
 from telegram.ext import ContextTypes
-from models.sentiment_model import get_sentiment
+from models import get_sentiment
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Send me text to get sentiment")
